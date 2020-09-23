5ed2f04fd127aacf4ba90aa54e2fc2ca186d6966
rajat <rajat4113@gmail.com>
2020-09-23 23:12:09 +0530

    front end

---
+++ public/precache-manifest.f3ae5c66d37ae1d7de47254c4e92f75f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247bbf4190396adb0b76a85cda22720e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e677eb0183df3df7f13",
+    "url": "/static/css/main.18627440.chunk.css"
+  },
+  {
+    "revision": "3d9f1fdc171c5b2eec1a",
+    "url": "/static/js/2.109602a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.109602a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e677eb0183df3df7f13",
+    "url": "/static/js/main.062efe10.chunk.js"
+  },
+  {
+    "revision": "8ef75fb86cb3d8079ece",
+    "url": "/static/js/runtime-main.a3052f89.js"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "aa0b5ae2488f49c233cd56f727c64e15",
+    "url": "/static/media/pencil.aa0b5ae2.svg"
+  }
+]);
