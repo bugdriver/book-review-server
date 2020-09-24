56150792e1e1e8abb5346440d224b9e4c0460a43
rajat <rajat4113@gmail.com>
2020-09-24 09:39:25 +0530

    added uuid in package.json

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,5 @@
 module.exports = {
-  books: `create table books(id SERIAL primary key,title varchar(255),writer varchar(255),front_image varchar(255));`,
+  books: `create table books(id SERIAL primary key,title varchar(255),writer varchar(255),front_image varchar(255),addedby varchar(100));`,
   reviews: `CREATE TABLE reviews(
     review_id SERIAL primary key,
     username varchar(255),
